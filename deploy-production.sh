98d35e4fcdcde068f55fd01ffdf9941a38ed049b
Pierre Genthon <genthon.pierre@gmail.com>
2024-03-07 18:20:58 +0100

    fix compose

--- deploy-production.sh
+++ deploy-production.sh
@@ -2,4 +2,4 @@
 git fetch origin && git reset --hard origin/production && git clean -f -d && \
 docker compose -f docker-compose.production.yml down && \
 docker compose -f docker-compose.production.yml pull && \
-docker compose -f docker-compose.production.yml --env-file ../.env.production up -d;
+docker compose -f docker-compose.production.yml --env-file .env.production up -d;
